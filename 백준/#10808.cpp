b3f46c1e5cd2f74b9b59749907ab714c6211b02f
DaeeYong <68097961+DaeeYong@users.noreply.github.com>
2021-10-18 14:57:00 +0900

    알바벳 개수
    시간복잡도: O(n)
    https://www.acmicpc.net/problem/10808

---
+++ 백준/#10808.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string arr;
+int alphabet[26] = { 0, };
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> arr;
+	for (int i = 0; i < arr.size(); i++) {
+		alphabet[arr[i] - 97] += 1;
+	}
+	
+	for (int i = 0; i < 26; i++) {
+		cout << alphabet[i] << ' ';
+	}
+	return 0;
+}
